0914a690b014c202f78317352d63d70c752b33d6
Mamo YZ <yzmamo@gmail.com>
2021-09-14 15:47:04 +0300

    page elements animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,32 @@
 $(document).ready(function () {
     $(".intro").addClass("init");
-
+    let introTimeout = 0;
     let fetch = setInterval(() => {
         if (document.readyState == "complete") {
             clearInterval(fetch);
             setTimeout(() => {
                 $(".intro").addClass("ready");
                 $("body").addClass("ready");
-            }, 4000);
+            }, introTimeout);
             setTimeout(() => {
                 $(".intro").fadeOut(1000);
-            }, 6000);
+                $(".hero").addClass("init");
+
+            }, introTimeout + 2000);
         }
     }, 200);
+    $(window).scroll(function (e) {
+        let winTop = $(window).scrollTop();
+        let co = ($(window).innerWidth() / $(window).innerHeight() < 0.8) ? 0.7 : 0.5
+        $.each($(".yz-animate"), function () {
+            if (winTop + $(window).innerHeight() * co > $(this).offset().top) {
+                $(this).addClass("init");
+            }
+        });
+        console.log((winTop + $(window).innerHeight()), $("body").innerHeight());
+
+        if ((winTop + $(window).innerHeight()) > $("body").innerHeight() - 100) {
+            $("footer").addClass("init");
+        }
+    })
 })
