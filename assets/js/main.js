658eb333ad0ed70f5ea91754dcee9f35bd7e9294
Mamo YZ <yzmamo@gmail.com>
2021-09-17 15:13:45 +0300

    revision

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $(".intro").addClass("init");
-    let introTimeout = 4000;
+    let introTimeout = 4000; //4000
+    let startDelay = 2000; //2000
+    let sliderDelay = 1000; //2000
     let fetch = setInterval(() => {
         if (document.readyState == "complete") {
             clearInterval(fetch);
@@ -12,9 +14,25 @@ $(document).ready(function () {
                 $(".intro").fadeOut(1000);
                 $(".hero").addClass("init");
 
-            }, introTimeout + 2000);
+            }, introTimeout + startDelay);
+            setTimeout(() => {
+                $(".hero .hero-bg .bg").eq(0).addClass("active");
+                initSlider();
+            }, introTimeout + startDelay + sliderDelay);
         }
     }, 200);
+    function initSlider() {
+        let slides = $(".hero .hero-bg .bg").length;
+        let activeSlide = -1;
+        let sliderInterval = setInterval(() => {
+            (activeSlide == slides - 2) ? activeSlide = -1 : activeSlide++;
+            $(".hero .hero-bg .bg.active").removeClass("active");
+            setTimeout(() => {
+                $(".hero .hero-bg .bg").eq(activeSlide + 1).addClass("active");
+            }, 800);
+
+        }, 4000);
+    }
     $(window).scroll(function (e) {
         let winTop = $(window).scrollTop();
         let co = ($(window).innerWidth() / $(window).innerHeight() < 0.8) ? 0.7 : 0.5
@@ -23,8 +41,6 @@ $(document).ready(function () {
                 $(this).addClass("init");
             }
         });
-        console.log((winTop + $(window).innerHeight()), $("body").innerHeight());
-
         if ((winTop + $(window).innerHeight()) > $("body").innerHeight() - 100) {
             $("footer").addClass("init");
         }
